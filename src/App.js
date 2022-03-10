115965e85d7bb93e358a83eb31697a3202852dc4
Marcelo Seiji <ikuta.marcelo@gmail.com>
2022-03-09 21:35:04 -0300

    initial files and folders added, babel and webpack basic configs added, yarn start and yarn build are working

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  return (
+    <div>
+      <h1 className="heading">React Boilerplate</h1>
+      <h4 className="sub-heading">No we can start out the project</h4>
+    </div>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+module.exports = {
+  // Entry point that indicates where
+  // should the webpack starts bundling
+  entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // checks for .js or .jsx files
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] }
+      },
+      {
+        test: /\.css$/, //checks for .css files
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+
+  // Options for resolving module requests
+  // extensions that are used
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+
+  // Output point is where webpack should
+  // output the bundles and assets
+  output: {
+    path: path.resolve(__dirname, 'public/dist'),
+    publicPath: '/public/dist/',
+    filename: 'bundle.js'
+  },
+
+  // Set the default por of webpack-dev-server
+  // from 8080 to 3000
+  devServer: {
+    port: 3000
+  }
+}
