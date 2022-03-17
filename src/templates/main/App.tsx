65d987b5756faa4391899a7787f5ffc38a438a09
Marcelo Seiji <ikuta.marcelo@gmail.com>
2022-03-17 20:17:51 -0300

    chore(configs): styled components added

--- src/templates/main/App.tsx
+++ src/templates/main/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import './App.css'
+
+import { AppContainer, AppTitle, AppSubtitle } from './styles'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="heading">React Boilerplate</h1>
-      <h4 className="sub-heading">Now we can start out the project</h4>
-    </div>
+    <AppContainer>
+      <AppTitle className="heading">React Boilerplate</AppTitle>
+      <AppSubtitle className="sub-heading">
+        Now we can start out the project
+      </AppSubtitle>
+    </AppContainer>
   )
 }
 
---
+++ src/templates/main/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const AppContainer = styled.div``
+
+export const AppTitle = styled.h1`
+  color: dodgerblue;
+  text-align: center;
+`
+
+export const AppSubtitle = styled.h2`
+  color: dodgerblue;
+  text-align: center;
+`
